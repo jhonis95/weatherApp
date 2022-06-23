5974fcbc58514b862daabc8ea1e81806a9acca56
jonatan de oliveira <j.jhonis95@gmail.com>
2022-06-22 23:17:35 -0400

    setting the APIs

--- src/home/home.js
+++ src/home/home.js
@@ -1,20 +1,21 @@
 import { Component, React, useState } from "react";
 import WeatherCard from "../comps/weatherCard";
 import styled from "styled-components";
+import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 
 const SearchBar=styled.input`
     width: 300px;
     background-color: aqua;
 `
-const BackgroundVideo=styled.div`
+const BackgroundVideo=styled.video`
     width: 100%;
-    height: 100%;
+    height: 100%;    
 `
 class home extends Component{
     constructor(pros){
         super(pros);
-        this.setState={
-            VideoLink:undefined
+        this.state={
+            VideoLink:''
         }
         this.getVideo=this.getVideo.bind(this)
     }
@@ -26,6 +27,9 @@ class home extends Component{
                 </div>
             </div>
         )
+    }
+    componentDidMount(){
+        
     }
     getVideo(){
         fetch('https://api.pexels.com/videos/videos/1860175',{
@@ -43,18 +47,14 @@ class home extends Component{
         }).then((data)=>{
             data.video_files.forEach(video => {
                 if(video.id===77347){
-                   this.setState({
-                       VideoLink:video.link
-                   })
+                    this.setState({
+                        VideoLink:video.link
+                    })
                 }
             });
-        }).then(
-            ()=>{
-                return this.state.VideoLink;
-            }
-        ).catch(
-            ()=>{
-                console.log('erro')
+        }).catch(
+            (erro)=>{
+                console.log(erro)   
             }
         )
     }
@@ -64,9 +64,11 @@ class home extends Component{
                 <SearchBar type="text" name="seatch"></SearchBar>
                 <button>search</button>
                 <WeatherCard />
-                <BackgroundVideo autoPlay loop muted url={this.getVideo}/>
+                {this.state.VideoLink===null?this.getVideo:this.state.VideoLink}
+                <BackgroundVideo id='video' autoPlay loop muted src={this.state.VideoLink}/>
             </section>
         )
     }
 }
+// console.log(()=>{})
 export default home;
